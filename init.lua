ca13debe92b9d57d26fce151cc4ee602274e80e4
Kaleb Alanis <kalebalanisdev@gmail.com>
2024-04-06 00:00:23 -0500

    update

--- init.lua
+++ init.lua
@@ -3,6 +3,8 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.o.number = true
+vim.g.mapleader = " "
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -21,18 +23,41 @@ vim.opt.rtp:prepend(lazypath)
 local plugins = {
   {"catppuccin/nvim", name = "catppuccin", priority = 1000 },
   {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+ { 'rust-lang/rust.vim' },
+    {
+        'mrcjkb/rustaceanvim',
+        version = '^3',
+        ft = { 'rust' },
+    },
+  {
+    "williamboman/mason.nvim"
+  },
   {
     'nvim-telescope/telescope.nvim', tag = '0.1.6',
     -- or                              , branch = '0.1.x',
       dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    }
   }
 }
 local opt = {}
 require("lazy").setup(plugins, opts)
 
+local builtin = require("telescope.builtin")
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
 local config = require("nvim-treesitter.configs")
 config.setup({
-  ensure_installed = {"lua", "javascript", "cpp"},
+  ensure_installed = {"nim", "lua", "javascript", "rust", "cpp", "astro"},
   highlight = {enable = true},
   indent = {enable = true}
 })
@@ -40,3 +65,15 @@ config.setup({
 require("catppuccin").setup()
 vim.cmd.colorscheme "catppuccin"
 
+require("mason").setup()
+
+local bufnr = vim.api.nvim_get_current_buf()
+vim.keymap.set(
+  "n", 
+  "<leader>a", 
+  function()
+    vim.cmd.RustLsp('codeAction') -- supports rust-analyzer's grouping
+    -- or vim.lsp.buf.codeAction() if you don't want grouping.
+  end,
+  { silent = true, buffer = bufnr }
+)
